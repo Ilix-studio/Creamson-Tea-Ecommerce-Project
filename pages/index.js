8f02b8eabc4b91b8d32bfd10e1c6bc96c5cd916d
Ilix-studio <ilishjyoti17@gmail.com>
2022-08-02 00:02:24 +0530

    Product component is made and data fetch from sanity db

--- pages/index.js
+++ pages/index.js
@@ -7,20 +7,22 @@ import { Product, FooterBanner, HeroBanner } from "../components";
 const Home = ({ products, bannerData }) => (
   <>
     <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
-   
+
     <div className="products-heading">
       <h2>Best Selling Product</h2>
       <p>Premium Collections</p>
     </div>
     <div className="products-container">
-      {products?.map((product) => product.name)}
+      {products?.map((product) => (
+        <Product key={product._id} product={product} />
+      ))}
     </div>
     <FooterBanner />
   </>
 );
 
-export const getServerSideProps = async() => {
-  // write sanity query equal to a string inside write * means fetch all type of products 
+export const getServerSideProps = async () => {
+  // write sanity query equal to a string inside write * means fetch all type of products
 
   const query = '*[_type == "product"]';
   const products = await client.fetch(query);
@@ -29,8 +31,8 @@ export const getServerSideProps = async() => {
   const bannerData = await client.fetch(bannerQuery);
 
   return {
-    props: { products, bannerData }
-  }
-}
+    props: { products, bannerData },
+  };
+};
 
 export default Home;
