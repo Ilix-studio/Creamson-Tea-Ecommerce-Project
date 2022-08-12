1c61b9ea103acf68862be1ea242f5f49c1390a79
Ilix-studio <ilishjyoti17@gmail.com>
2022-08-12 08:45:54 +0530

    added slug && made them moving

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -7,6 +7,7 @@ import {
 } from "react-icons/ai";
 
 import { client, urlFor } from "../../lib/client";
+import { Product } from '../../components'
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
@@ -23,16 +24,54 @@ const ProductDetails = ({ product, products }) => {
             ))}
           </div> */}
         </div>
-        <div className="product-detaild-desc">
+        <div className="product-detail-desc">
           <h1>{name}</h1>
           <div className="reviews">
             <div>
-                <AiFillStar />
-                <AiFillStar />
-                <AiFillStar />
-                <AiFillStar />
-                <AiOutlineStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
             </div>
+            <p>(20)</p>
+          </div>
+          <h4>Details:</h4>
+          <p>{details}</p>
+          <p className="price">₹{price}</p>
+          <div className="quantity">
+            <h3>Quantity:</h3>
+            <p className="quantity-desc">
+              <span className="minus" onClick="">
+                {" "}
+                <AiOutlineMinus />
+              </span>
+              <span className="num" onClick="">
+                {" "}
+                0
+              </span>
+              <span className="plus" onClick="">
+                {" "}
+                <AiOutlinePlus />
+              </span>
+            </p>
+          </div>
+          <div className="buttons">
+            <button type="button" className="add-to-cart" onClick="">
+              Add to Cart
+            </button>
+            <button type="button" className="buy-now" onClick="">
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* scolling products  */}
+      <div className="maylike-products-wrapper">
+        <h2>You may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item) => (<Product key={item.id} product={item} />))}
           </div>
         </div>
       </div>
