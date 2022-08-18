86a770ec5804b2f07fa1891e74a16cb6c966c112
Ilix-studio <ilishjyoti17@gmail.com>
2022-08-18 15:20:11 +0530

    update and remove item functionality were done

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -13,8 +13,14 @@ import { urlFor } from "../lib/client";
 
 const Cart = () => {
   const cartRef = useRef();
-  const { totalPrice, totalQuantities, cartItems, setShowCart } =
-    useStateContext();
+  const {
+    totalPrice,
+    totalQuantities,
+    cartItems,
+    setShowCart,
+    toggleCartItemQuanitity,
+    onRemove,
+  } = useStateContext();
   // jsx for cart Container
   return (
     <div className="cart-wrapper" ref={cartRef}>
@@ -51,7 +57,10 @@ const Cart = () => {
           {cartItems.length >= 1 &&
             cartItems.map((item) => (
               <div className="product" key={item._id}>
-                <img src={urlFor(item?.image[0])} className="cart-product-image" />
+                <img
+                  src={urlFor(item?.image[0])}
+                  className="cart-product-image"
+                />
                 <div className="item-desc">
                   <div className="flex top">
                     <h5>{item.name}</h5>
@@ -60,18 +69,32 @@ const Cart = () => {
                   <div className="flex button">
                     <div>
                       <p className="quantity-desc">
-                        <span className="minus" onClick="">
+                        <span
+                          className="minus"
+                          onClick={() =>
+                            toggleCartItemQuanitity(item._id, "dec")
+                          }
+                        >
                           <AiOutlineMinus />
                         </span>
                         <span className="num" onClick="">
-                          0
+                          {item.quantity}
                         </span>
-                        <span className="plus" onClick="">
+                        <span
+                          className="plus"
+                          onClick={() =>
+                            toggleCartItemQuanitity(item._id, "inc")
+                          }
+                        >
                           <AiOutlinePlus />
                         </span>
                       </p>
                     </div>
-                    <button type="button" className="remove-item" onClick="">
+                    <button
+                      type="button"
+                      className="remove-item"
+                      onClick={() => onRemove(item)}
+                    >
                       <TiDeleteOutline />
                     </button>
                   </div>
@@ -80,17 +103,20 @@ const Cart = () => {
             ))}
         </div>
         {/* subtotal logic */}
-         {cartItems.length >= 1 && (
+        {cartItems.length >= 1 && (
           <div className="cart-botton">
             <div className="total">
               <h3>Subtotal:</h3>
               <h3>{totalPrice}</h3>
             </div>
             <div className="btn-container">
-              <button type="button" className="btn" onClick=""> Pay with Stripe</button>
+              <button type="button" className="btn" onClick="">
+                {" "}
+                Pay with Stripe
+              </button>
             </div>
           </div>
-         )}
+        )}
       </div>
     </div>
   );
--- context/StateContext.js
+++ context/StateContext.js
@@ -10,6 +10,9 @@ export const StateConext = ({ children }) => {
   const [totalQuantities, setTotalQuantities] = useState(0);
   const [qty, setQty] = useState(1);
 
+  let foundProduct;
+  let index;
+
   const incQty = () => {
     setQty((prevQty) => prevQty + 1);
   };
@@ -45,6 +48,38 @@ export const StateConext = ({ children }) => {
     toast.success(`${qty} ${product.name} added to the cart.`);
   };
 
+  const toggleCartItemQuanitity = (id, value) => {
+    foundProduct = cartItems.find((item) => item._id === id);
+    index = cartItems.findIndex((product) => product._id === id);
+    const newCartItems = cartItems.filter((item) => item._id !== id);
+
+    if (value === "inc") {
+      setCartItems([
+        ...newCartItems,
+        { ...foundProduct, quantity: foundProduct.quantity + 1 },
+      ]);
+      setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price);
+      setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + 1);
+    } else if (value === "dec") {
+      if (foundProduct.quantity > 1) {
+        setCartItems([
+          ...newCartItems,
+          { ...foundProduct, quantity: foundProduct.quantity - 1 },
+        ]);
+        setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price);
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - 1);
+      }
+    }
+  };
+  const onRemove = (product) => {
+    foundProduct = cartItems.find((item) => item._id === product._id);
+    const newCartItems = cartItems.filter((item) => item._id !== product._id);
+
+    setTotalPrice((prevTotalPrice) => prevTotalPrice -foundProduct.price * foundProduct.quantity);
+    setTotalQuantities(prevTotalQuantities => prevTotalQuantities - foundProduct.quantity);
+    setCartItems(newCartItems);
+  }
+
   return (
     <Context.Provider
       value={{
@@ -57,6 +92,8 @@ export const StateConext = ({ children }) => {
         incQty,
         decQty,
         onAdd,
+        toggleCartItemQuanitity,
+        onRemove,
       }}
     >
       {children}
